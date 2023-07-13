e9a3f624b33646096c30f821f20059c2d1c4b44c
DvlprMrn <rplc.999@outlook.com>
2023-07-13 18:00:31 -0400

    fixed

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSearchQuery, setSearchResults } from '../../redux/slices';
 import { RootState } from '../../redux';
@@ -9,20 +9,11 @@ import './Home.css';
 const Home: React.FC = () => {
     const dispatch = useDispatch();
     const searchQuery = useSelector((state: RootState) => state.home.searchQuery);
-    const inputRef = useRef<HTMLInputElement>(null);
-    const [isInputFocused, setIsInputFocused] = useState(false);
 
     const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         dispatch(setSearchQuery(e.target.value));
     };
 
-    const handleInputTouchStart = () => {
-        setIsInputFocused(true);
-    };
-
-    const handleInputTouchEnd = () => {
-        setIsInputFocused(false);
-    };
 
     useEffect(() => {
         const fetchSearchResults = async () => {
@@ -51,25 +42,23 @@ const Home: React.FC = () => {
         <div className="home">
             <div className="input-container">
                 <form className="search">
-                    <input
-                        type="text"
-                        value={searchQuery}
-                        onChange={handleSearchInputChange}
-                        placeholder="Search by movie name"
-                        ref={inputRef}
-                        onTouchStart={handleInputTouchStart}
-                        onTouchEnd={handleInputTouchEnd}
-                        className={isInputFocused ? 'focused' : ''}
-                    />
+                    <a href="#" className="search-link">
+                        <input
+                            type="text"
+                            value={searchQuery}
+                            onChange={handleSearchInputChange}
+                            placeholder="Search by movie name"
+                        />
+                    </a>
                 </form>
             </div>
+
             {searchQuery && <SearchResults />}
         </div>
     );
 };
 
 export default Home;
-
 
 
 
